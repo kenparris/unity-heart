68a530038959849057ff3379ea875f6152e7b717
Ken <ken@MelkBook-5.local>
2026-02-14 00:33:53 -0500

    add LUV lerp

--- Assets/Scripts/Lerp1.cs
+++ Assets/Scripts/Lerp1.cs
@@ -1,8 +1,8 @@
 // UMD IMDM290 
 // Instructor: Myungin Lee
-    // [a <-----------> b]
-    // Lerp : Linearly interpolates between two points. 
-    // https://docs.unity3d.com/6000.3/Documentation/ScriptReference/Vector3.Lerp.html
+// [a <-----------> b]
+// Lerp : Linearly interpolates between two points. 
+// https://docs.unity3d.com/6000.3/Documentation/ScriptReference/Vector3.Lerp.html
 
 using UnityEngine;
 
@@ -10,29 +10,59 @@ public class Lerp1 : MonoBehaviour
 {
     GameObject[] spheres;
     static int numSphere = 200; 
-    float time = 0f;
-    Vector3[] startPosition, endPosition;
+    float time = 3f * Mathf.PI / 2f, prevLerp = 0f;
+    int offset = 0;
+    bool nadir = true;
+    Vector3[] startPosition, endPositions;
 
     // Start is called before the first frame update
     void Start()
     {
         // Assign proper types and sizes to the variables.
         spheres = new GameObject[numSphere];
-        startPosition = new Vector3[numSphere]; 
-        endPosition = new Vector3[numSphere]; 
+        startPosition = new Vector3[numSphere];
+        endPositions = new Vector3[4 * numSphere];
         
         // Define target positions. Start = random, End = heart 
         for (int i =0; i < numSphere; i++){
             // Random start positions
             float r = 10f;
             startPosition[i] = new Vector3(r * Random.Range(-1f, 1f), r * Random.Range(-1f, 1f), r * Random.Range(-1f, 1f));        
+            
+            float t = i / (float)numSphere;
+            float cos = Mathf.Cos(2 * Mathf.PI * t);
+            float sin = Mathf.Sin(2 * Mathf.PI * t);
+            r = 3f;
 
-            r = 3f; // radius of the circle
-            float cos = Mathf.Cos(i * 2 * Mathf.PI / numSphere);
-            float sin = Mathf.Sin(i * 2 * Mathf.PI / numSphere);
+            //L
+            if (i < numSphere / 3f) {
+                endPositions[i] = new Vector3(r * (4 * t - (2f / 3f)), -2f);
+            } 
+            else {
+                endPositions[i] = new Vector3(r * -(2f / 3f), r * (3 * t - (5f / 3f)));
+            }
+
+            // U
+            if (i < numSphere / 3f)
+            {
+                endPositions[i + numSphere] = new Vector3(-2f, r * 4 * t);
+            } else if (i < 2f * numSphere / 3f) {
+                endPositions[i + numSphere] = new Vector3(-2f * Mathf.Cos(3 * Mathf.PI * (t - (1f / 3f))), -2f * Mathf.Sin(3 * Mathf.PI * (t - (1f / 3f))));
+            } else {
+                endPositions[i + numSphere] = new Vector3(2f, r * (4 * t - (8f / 3f)));
+            }
+
+            // V
+            if (i < numSphere / 2f) {
+                endPositions[i + numSphere * 2] = new Vector3(r * (4f / 3f * t - (2f / 3f)), r * (-4f * t + (4f / 3f)));
+            } else {
+                endPositions[i + numSphere * 2] = new Vector3(r * (4f / 3f * t - (2f / 3f)), r * (4f * t - (8f / 3f)));
+            }
+
+
             // Circular end position
             // endPosition[i] = new Vector3(r * Mathf.Sin(i * 2 * Mathf.PI / numSphere), r * Mathf.Cos(i * 2 * Mathf.PI / numSphere));
-            endPosition[i] = new Vector3(
+            endPositions[i + numSphere * 3] = new Vector3(
                 r * Mathf.Sqrt(2) * Mathf.Pow(sin, 3), 
                 r * (2 * cos - Mathf.Pow(cos, 3) - Mathf.Pow(cos, 2)) + 2
             );
@@ -61,16 +91,26 @@ public class Lerp1 : MonoBehaviour
         // Measure Time 
         time += Time.deltaTime; // Time.deltaTime = The interval in seconds from the last frame to the current one
         // what to update over time?
+
+        // lerpFraction variable defines the point between startPosition and endPosition (0~1)
+        // let it oscillate over time using sin function
+        float lerpFraction = Mathf.Sin(time) * 0.5f + 0.5f;
+
+        if (!nadir && lerpFraction - prevLerp > 0) { //when lerpFraction starts to increase (toward end position)
+            nadir = true;
+            offset = offset + numSphere >= numSphere * 4 ? 0 : offset + numSphere;
+        } else if (nadir && lerpFraction - prevLerp < 0) { //when lerpFraction starts to decrease (toward start position)
+            nadir = false;
+        }
+        prevLerp = lerpFraction;
+
         for (int i =0; i < numSphere; i++){
             // Lerp : Linearly interpolates between two points.
             // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/Vector3.Lerp.html
             // Vector3.Lerp(startPosition, endPosition, lerpFraction)
-            
-            // lerpFraction variable defines the point between startPosition and endPosition (0~1)
-            // let it oscillate over time using sin function
-            float lerpFraction = Mathf.Sin(time) * 0.5f + 0.5f;
+
             // Lerp logic. Update position
-            spheres[i].transform.position = Vector3.Lerp(startPosition[i], endPosition[i], lerpFraction);
+            spheres[i].transform.position = Vector3.Lerp(startPosition[i], endPositions[i + offset], lerpFraction);
             // For now, start positions and end positions are fixed. But what if you change it over time?
             // startPosition[i]; endPosition[i];
 
